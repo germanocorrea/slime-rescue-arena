14ee3bf725fef499070dd82601dadb733e5b4380
Germano Corrêa <germanobruscato@gmail.com>
2026-05-23 18:16:12 -0300

    wip: added bounce cooldown to allow bouncing on walls

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -5,11 +5,30 @@ const SPEED = 600.0
 const JUMP_VELOCITY = -900.0
 const BOUNCE_STRENGTH = 0.8
 
+var bounce_cooldown := 0.0
+
 func _physics_process(delta: float) -> void:
 	process_movement(delta)
 	var before_slide_velocity := velocity
 	move_and_slide()
 	bounce_response(before_slide_velocity)
+
+func process_movement(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	if bounce_cooldown > 0:
+		bounce_cooldown -= delta
+		return
+
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 func bounce_response(before_slide_velocity: Vector2) -> void:
 	if Input.is_action_pressed("ui_down"):
@@ -20,24 +39,7 @@ func bounce_response(before_slide_velocity: Vector2) -> void:
 		if not collision:
 			continue
 
-		before_slide_velocity = before_slide_velocity.bounce(collision.get_normal()) * BOUNCE_STRENGTH
-		var bounce_diff := before_slide_velocity.length() - velocity.length()
-		if bounce_diff > 200:
-			velocity = before_slide_velocity
+		velocity = before_slide_velocity.bounce(collision.get_normal()) * BOUNCE_STRENGTH
 
-func process_movement(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-#
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	if not is_on_floor():
-		return
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		if abs(collision.get_normal().x) > 0.5:
+			bounce_cooldown = 0.2
