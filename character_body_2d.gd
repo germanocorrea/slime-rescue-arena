2849fc9943edfba80863881a27a839549244a291
Germano Corrêa <germanobruscato@gmail.com>
2026-05-23 18:27:45 -0300

    wip: decople on_floor function for future change and general code organization

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -13,22 +13,29 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	bounce_response(before_slide_velocity)
 
-func process_movement(delta: float) -> void:
-	if not is_on_floor():
+func is_just_enough_close_to_floor():
+	return is_on_floor()
+
+func process_jump(delta: float) -> void:
+	if not is_just_enough_close_to_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_just_enough_close_to_floor():
 		velocity.y = JUMP_VELOCITY
 
-	if bounce_cooldown > 0:
-		bounce_cooldown -= delta
-		return
-
+func process_lateral_movement() -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+func process_movement(delta: float) -> void:
+	process_jump(delta)
+	if bounce_cooldown > 0:
+		bounce_cooldown -= delta
+		return
+	process_lateral_movement()
 
 func bounce_response(before_slide_velocity: Vector2) -> void:
 	if Input.is_action_pressed("ui_down"):
