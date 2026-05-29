2eb1db12483224608db2c925cd2e79d99936ae08
Germano Bruscato Corrêa <germanobruscato@gmail.com>
2026-05-29 18:09:14 -0300

    final movement version

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
+const MAX_SPEED = 600.0
 const JUMP_VELOCITY = -900.0
 const BOUNCE_STRENGTH = 0.8
+const ACCELERATION = 1200.0
+const FRICTION = 1450.0
 
 var bounce_cooldown := 0.0
 
@@ -11,39 +13,38 @@ func _physics_process(delta: float) -> void:
 	process_movement(delta)
 	var before_slide_velocity := velocity
 	move_and_slide()
-	bounce_response(before_slide_velocity)
-
-func is_just_enough_close_to_floor():
-	return is_on_floor()
+	bounce_response(before_slide_velocity, delta)
 
 func process_jump(delta: float) -> void:
-	if not is_just_enough_close_to_floor():
+	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_pressed("ui_up") and is_just_enough_close_to_floor():
+	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-func process_lateral_movement() -> void:
+func process_lateral_movement(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, direction * MAX_SPEED, ACCELERATION * delta)
 
 func process_movement(delta: float) -> void:
 	process_jump(delta)
 	if bounce_cooldown > 0:
 		bounce_cooldown -= delta
 		return
-	process_lateral_movement()
+	process_lateral_movement(delta)
 
-func bounce_response(before_slide_velocity: Vector2) -> void:
-	if Input.is_action_pressed("ui_down"):
-		return
+func bounce_response(before_slide_velocity: Vector2, delta: float) -> void:
+	var bounce_breaks := Input.is_action_pressed("ui_down")
 
 	for i in get_slide_collision_count():
 		var collision := get_slide_collision(i)
 		if not collision:
+			continue
+
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
+		if bounce_breaks:
 			continue
 
 		velocity = before_slide_velocity.bounce(collision.get_normal()) * BOUNCE_STRENGTH
